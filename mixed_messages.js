151bc28e0a91a0bc141c9bbd8f1df913c9d02aa4
Callum Wishart <cgawishart@gmail.com>
2022-04-28 07:26:02 +1200

    Add script generator

--- mixed_messages.js
+++ mixed_messages.js
@@ -1,12 +1,16 @@
 const house_colour = ["blue", "green", "grey", "yellow", "red", "purple", "black", "white", "rainbow"];
 const house_type = ["villa", "modern", "brick", "hay", "mansion", "apartment", "penthouse", "boat", "car"];
-const family = ["cats", "dogs", "children", "hamster", "alligator"];
-const car = ["ferrari", "toyota", "motorbike", "bike", "bus", "legs"];
+const family = ["cat", "dog", "kid", "hamster", "alligator"];
+const car = ["ferrari", "toyota", "motorbike", "bike", "bus card", "legs"];
 
-const randNumGen = () => {
-    const houseColourNum =  Math.floor(Math.random() * 9);
-    const houseTypeNum = Math.floor(Math.random() * 9);
-    const familyNum = Math.floor(Math.random() * 5);
-    const carNum = Math.floor(Math.random() * 6);
-}
 
+const houseColourNum =  Math.floor(Math.random() * 9);
+const houseTypeNum = Math.floor(Math.random() * 9);
+const familyNum = Math.floor(Math.random() * 5);
+const carNum = Math.floor(Math.random() * 6);
+
+console.log("Today I will predict your future...");
+console.log("Through looking into your eyes I am able to predict four aspects of your future");
+console.log("The first is that you will live in a " + house_colour[houseColourNum] + " " + house_type[houseTypeNum] + ".");
+console.log("I predict a fammily with " + Math.floor(Math.random() * 10) + " " + family[familyNum] + "'s.");
+console.log("To get you around town you will use your " + car[carNum]);
