d15c273b7b8fac1ba914bcd0cad14b110ef0178c
Callum Wishart <cgawishart@gmail.com>
2022-04-28 07:16:01 +1200

    Add random number generator for each component

--- mixed_messages.js
+++ mixed_messages.js
@@ -3,5 +3,10 @@ const house_type = ["villa", "modern", "brick", "hay", "mansion", "apartment", "
 const family = ["cats", "dogs", "children", "hamster", "alligator"];
 const car = ["ferrari", "toyota", "motorbike", "bike", "bus", "legs"];
 
-
+const randNumGen = () => {
+    const houseColourNum =  Math.floor(Math.random() * 9);
+    const houseTypeNum = Math.floor(Math.random() * 9);
+    const familyNum = Math.floor(Math.random() * 5);
+    const carNum = Math.floor(Math.random() * 6);
+}
 
