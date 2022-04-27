0ea62eec0314da75a9b7e98a84b279e542228eba
Callum Wishart <cgawishart@gmail.com>
2022-04-27 22:30:32 +1200

    Add message components

--- mixed_messages.js
+++ mixed_messages.js
@@ -1,1 +1,7 @@
-console.log("Hello World!");
+const house_colour = ["blue", "green", "grey", "yellow", "red", "purple", "black", "white", "rainbow"];
+const house_type = ["villa", "modern", "brick", "hay", "mansion", "apartment", "penthouse", "boat", "car"];
+const family = ["cats", "dogs", "children", "hamster", "alligator"];
+const car = ["ferrari", "toyota", "motorbike", "bike", "bus", "legs"];
+
+
+
